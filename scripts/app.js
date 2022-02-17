8f55fc23f601a1234342f95ca4e2ab14a4341330
Md Alamin <vijayalamin@gmail.com>
2022-02-17 03:18:26 -0800

    final commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,9 @@
-function calculate() {
-    const icomeErrorText = getVariable('error-text');
-    const expenseTotalSpan = getVariable('expenseTotal');
-    const balanceLeft = getVariable('balanceLeft');
-    const food = getInput('food');
-    const rent = getInput('rent');
-    const clothes = getInput('clothes');
-    const incomeInput = getInput('income');
-    const expenseTotal = food + clothes + rent;
-
-
-    // validation and setting innertext 
-
-    
-    if (isNaN(incomeInput) == false && incomeInput > 0) {
-        if (expenseTotal > incomeInput) {
-            alert('not Enough to spend')
-        } else if (expenseTotal >= 0) {
-            expenseTotalSpan.innerText = expenseTotal;
-            balanceLeft.innerText = incomeInput - expenseTotal;
-        }
-    } else {
-        icomeErrorText.style.display = 'block';
-    }
-
-}
-
 
 // this function is for gettting input and its value
 function getInput(id) {
     const inputValue = document.getElementById(id);
     const inputValueNum = inputValue.value;
-    inputValue.value = ""
+    // inputValue.value = "";
     return parseInt(inputValueNum);
 }
 
@@ -40,16 +13,68 @@ function getVariable(id) {
     return field;
 }
 
-// this function is for savings button
-function savings() {
+// function  for food rent cloths
+function expense() {
+
+    const food = getInput('food');
+    const rent = getInput('rent');
+    const clothes = getInput('clothes');
+    const expenseTotal = food + clothes + rent;
+    return expenseTotal;
+}
+function calculate() {
+    // income
     const incomeInput = getInput('income');
-    const savingsAmount = incomeInput * (getInput('save') / 100);
-    //validation
-    if (savingsAmount <= incomeInput && savingsAmount > 0) {
-        getVariable('savings').innerText = savingsAmount;
-        getVariable('remaining').innerText = incomeInput - savingsAmount;
-        getVariable('savingsError').style.display = 'none'
-    } else {
-        getVariable('savingsError').style.display = 'block'
+    const icomeErrorText = getVariable('error-text');
+    //balance
+    const balanceLeftSpan = getVariable('balanceLeft');
+
+    // expenses details 
+    const expenseTotal = expense();
+    const expenseTotalSpan = getVariable('expenseTotal');
+    //logics 
+    if (incomeInput >= expenseTotal) {
+        balanceLeftSpan.innerText = incomeInput - expenseTotal;
+        expenseTotalSpan.innerText = expenseTotal;
+    } else if (expenseTotal > incomeInput) {
+        alert('not Enough to spend')
     }
+    else {
+        getVariable('error-text').style.display = 'block'
+    }
+
+}
+
+function savings() {
+    const expenseTotal = expense();
+    const remainingBalanceSpan = getVariable('remaining');
+    const incomeInput = getInput('income');
+    const savingsAmountSpan = getVariable('savings');
+    const savingsAmount = incomeInput * (getInput('save') / 100);
+
+    // setting and validatting remaining amount 
+    if (getInput('save') > 0 && incomeInput > 0) {
+        if (expenseTotal <= 0 || isNaN(expenseTotal) == true) {
+            remainingBalanceSpan.innerText = incomeInput - savingsAmount;
+            // setting and validating saving amount
+            savingsAmountSpan.innerText = savingsAmount;
+        } else {
+
+            if (incomeInput - expenseTotal - savingsAmount > 0) {
+                if (getInput('save') < 100) {
+                    remainingBalanceSpan.innerText = incomeInput - expenseTotal - savingsAmount;
+                }else{
+                    alert('cant save more than 100%')
+                }
+                // setting and validating saving amount
+                savingsAmountSpan.innerText = savingsAmount;
+            } else {
+                getVariable('savingsError').style.display = 'block'
+            }
+        }
+    } else {
+        alert('amount cant be less than 0')
+    }
+
+
 }
